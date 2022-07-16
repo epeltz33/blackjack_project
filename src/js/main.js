8cae5afbccaeb719127758eecd9f23d5b400be9c
Eric-Peltzman33 <erpeltz@gmail.com>
2022-07-16 16:55:23 -0400

    player variables and the player class

---
+++ src/js/main.js
@@ -0,0 +1,41 @@
+// making a blackjack game with javascript, html, and css
+
+
+let playerBank = 0; // player's bank amount
+let playerBet = 50; // player's bet amount (default is 50) 
+let coveredCard; // card that is covered by the dealer
+
+
+// player model with properties and methods for player object
+class Player {
+  // constructor function for player object
+  constructor() {
+    this.coveredCard = [];
+    this.hand = []; // player's hand
+    this.active = true; // player's active status
+  }
+  
+  }
+
+	
+	  
+	 
+		  
+		  
+	    
+	    
+	   
+	   
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+
+
+
