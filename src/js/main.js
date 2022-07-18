52114a041c380c93c0a69fd32368aea79f9434c4
Eric-Peltzman33 <erpeltz@gmail.com>
2022-07-18 15:31:16 -0400

    if else statments

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,22 @@
-  
+   
+  let firstCard = 10 
+   let secondCard = 11
+   let sum = secondCard + firstCard
+   let haveBlackJack = false
+// 1. make a variable called isAlive and assign it to true 
+   let isAlive = true
+// 2. flip its value to false in the appropriate code block 
+if ( sum <= 20 ) {
+  console.log( "Do you want to hit?" )
+} else if (sum === 21) {
+ console.log("You got Blackjack!")
+  haveBlackJack = true;
+ } else {
+    console.log("You lost!")
+   isAlive = false
+  }
+console.log(isAlive)
+    
+// Cash Out
+
+
