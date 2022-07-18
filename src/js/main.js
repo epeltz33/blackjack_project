4f8d7ede47fc4a51e45d7d0b9fdda9d65aec359e
Eric-Peltzman33 <erpeltz@gmail.com>
2022-07-18 15:41:19 -0400

    message

--- src/js/main.js
+++ src/js/main.js
@@ -1,22 +1,27 @@
    
   let firstCard = 10 
+  
    let secondCard = 11
-   let sum = secondCard + firstCard
+   
+   let sum = secondCard + firstCard + secondCard;
+   
    let haveBlackJack = false
-// 1. make a variable called isAlive and assign it to true 
+   
+   let message = ''
+//  make a variable called isAlive and assign it to true 
    let isAlive = true
-// 2. flip its value to false in the appropriate code block 
+// flip its value to false in the appropriate code block 
 if ( sum <= 20 ) {
-  console.log( "Do you want to hit?" )
+  message = "Do you want to hit?"; 
 } else if (sum === 21) {
- console.log("You got Blackjack!")
+  message = "You got Blackjack!";
   haveBlackJack = true;
  } else {
-    console.log("You lost!")
+  message = "You busted!";
    isAlive = false
   }
-console.log(isAlive)
+console.log(message);
     
-// Cash Out
+
 
 
