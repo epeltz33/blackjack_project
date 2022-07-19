b9a8918bb6c40fa0c3f7088b65a8291e9942fca2
epeltz33 <peltzmae@go.stockton.edu>
2022-07-19 21:37:29 +0000

    randomCard working

--- src/js/main.js
+++ src/js/main.js
@@ -1,49 +1,59 @@
-   
-  let firstCard = 7;
+let firstCard = getRandomCard();
 
-  let secondCard = 8;
+let secondCard = getRandomCard();
 
-  let sum = secondCard + firstCard;
+let sum = secondCard + firstCard;
 
-  let haveBlackJack = false;
+let haveBlackJack = false;
 
-  let message = "";
+let message = "";
 
-  let messageEl = document.getElementById("message-el");
+let messageEl = document.getElementById("message-el");
 
-  let totalEl = document.querySelector("#total-el");
+let totalEl = document.querySelector("#total-el");
 
-  let cardsEl = document.querySelector("#cards-el");
+let cardsEl = document.querySelector("#cards-el");
 
-  let cards = [firstCard, secondCard];
+let cards = [firstCard, secondCard];
 
-  let isAlive = true;
-  const startGame = () => {
-    renderGame(); // calls startGame
-  };
+let isAlive = true;
 
-  const renderGame = () => {
-    cardsEl.textContent = `Cards: ${cards[0]}  ${cards[1]} `;
-    // render out All the cards we have
-    totalEl.textContent = `Total: ${sum} `;
-    if (sum <= 20) {
-      message = "Do you want to hit?";
-    } else if (sum === 21) {
-      message = "You got Blackjack!";
-      haveBlackJack = true;
-    } else {
-      message = "You busted!";
-      isAlive = false;
-    }
-    messageEl.textContent = message;
-  };
+function getRandomCard() {
+  let card = Math.floor(Math.random() * (11 - 1 + 1)) + 1; // random number between 1 and 11
 
-  const newCard = () => {
-    console.log(`Drawing a new card from the deck`);
-    let card = 6;
-    sum += card;
-    // Push the card to the cards array
-    cards.push(card);
-    console.log(cards);
-    renderGame();
-  };
+  return card;
+}
+
+let startGame = () => {
+  renderGame(); // calls startGame
+};
+
+let renderGame = () => {
+  cardsEl.textContent = "Cards:  ";
+  for (let i = 0; i < cards.length; i++) {
+    cardsEl.textContent += cards[i] + " ";
+  }
+  totalEl.textContent = `Total: ${sum} `;
+  if (sum <= 20) {
+    message = "Do you want to hit?";
+  } else if (sum === 21) {
+    message = "You got Blackjack!";
+    haveBlackJack = true;
+  } else {
+    message = "You busted!";
+    isAlive = false;
+  }
+  messageEl.textContent = message;
+};
+
+const newCard = () => {
+  console.log(`Drawing a new card from the deck`);
+  let card = getRandomCard();
+  sum += card;
+  // Push the card to the cards array
+  cards.push(card);
+
+  console.log(cards);
+
+  renderGame();
+};
