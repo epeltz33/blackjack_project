b9ea96fa784d79f5d8c5aa74dbe4938f1773ec11
epeltz33 <peltzmae@go.stockton.edu>
2022-07-19 01:32:18 +0000

    made the play button

--- src/js/main.js
+++ src/js/main.js
@@ -3,7 +3,7 @@
   
    let secondCard = 11
    
-   let sum = secondCard + firstCard + secondCard;
+   let sum = secondCard + firstCard; 
    
    let haveBlackJack = false
    
@@ -11,16 +11,18 @@
 //  make a variable called isAlive and assign it to true 
    let isAlive = true
 // flip its value to false in the appropriate code block 
-if ( sum <= 20 ) {
-  message = "Do you want to hit?"; 
-} else if (sum === 21) {
-  message = "You got Blackjack!";
-  haveBlackJack = true;
- } else {
-  message = "You busted!";
-   isAlive = false
+const startGame = () => {
+  if (sum <= 20) {
+    message = "Do you want to hit?";
+  } else if (sum === 21) {
+    message = "You got Blackjack!";
+    haveBlackJack = true;
+  } else {
+    message = "You busted!";
+    isAlive = false;
   }
-console.log(message);
+  console.log(message);
+};
     
 
 
