ad7b3f63bd8c0b09d48cdb1566e8b573b9662875
Eric-Peltzman33 <erpeltz@gmail.com>
2022-07-20 20:14:11 -0400

    event listeners

--- src/js/main.js
+++ src/js/main.js
@@ -1,69 +1,27 @@
-let player = {
-  playerName: "Per",
-  playerChips: 145,
-};
-//
-let isAlive = false;
-let haveBlackJack = false;
-let sum = 0;
-let message = "";
-let cards = [];
-// DOM elements
-let messageEl = document.getElementById("message-el");
-let totalEl = document.querySelector("#total-el");
-let cardsEl = document.querySelector("#cards-el");
-let playerEl = document.getElementById("player-el");
-playerEl.textContent = `Player: ${player.playerName} Chips: ${player.playerChips}`;
-console.log(playerEl);
+// blackjack game global variables
 
-console.log(cards);
-function getRandomCard() {
-  let randomNumber = Math.floor(Math.random() * 13) + 1; // random number between 1 and 13 (made a mistake here)
+let deck = [];
+let cash = 100;
+let playerCards = [];
+let splitCards = [];
+let dealerCards = [];
+let dealerCount = 0;
+let playerCount = 0;
+let splitCount = 0;
+let playerBet = 0;
+let splitBet = 0;
+let currentCards = "";
 
-  return randomNumber > 10 ? 10 : randomNumber === 1 ? 11 : randomNumber;
-}
+cashELement();
 
-let startGame = () => {
-  isAlive = true;
-  let firstCard = getRandomCard();
+// event listeners for buttons
 
-  let secondCard = getRandomCard();
-
-  cards = [firstCard, secondCard];
-   sum = secondCard + firstCard;
-
-  renderGame();
-};
-
-let renderGame = () => {
-  cardsEl.textContent = "Cards:  ";
-
-  for (let i = 0; i < cards.length; i++) {
-    cardsEl.textContent += cards[i] + " ";
-  }
-  totalEl.textContent = `Total: ${sum} `;
-
-  if (sum <= 20) {
-    message = "Do you want to hit?";
-  } else if (sum === 21) {
-    message = "You got Blackjack!";
-
-    haveBlackJack = true;
-  } else {
-    message = "Your out of the game! You busted!";
-
-    isAlive = false;
-  }
-  messageEl.textContent = message;
-};
-
-function newCard () {
-	if ( isAlive === true && haveBlackJack === false ) {
-		let card = getRandomCard();
-
-		sum += card;
-		// Push the card to the cards array
-		cards.push( card );
-
-	}
-}
+documentqueryselector("#hit-button").addeventlistener("click", cardTake);
+documentqueryselector("#stand-button").addeventlistener("click", dealerPlay);
+documentqueryselector("#double-button").addeventlistener("click", doubleDown);
+documentqueryselector("#play-again-button").addeventlistener("click",playAgain);
+documentqueryselector("#split-button").addeventlistener("click", split);
+documentqueryselector("#bet-button").addeventlistener("click", bet);
+documentqueryselector("#reset-button").addeventlistener("click", reset);
+documentqueryselector("#clear-button").addeventlistener("click", clear);
+documentqueryselector("#deal-button").addeventlistener("click", deal);
