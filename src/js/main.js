cb84a716d0ee678a06c5e8824e98ffdc34adccac
Eric-Peltzman33 <erpeltz@gmail.com>
2022-07-20 22:11:44 -0400

    a lot of functions almost done

--- src/js/main.js
+++ src/js/main.js
@@ -100,3 +100,185 @@ function clearBet () {
 	$( "#deal-button, #clear-button, .form-inline" ).toggle();
 }
 
+// deal the cards function
+
+ function dealTheHand() {
+ shuffleDeck();
+	 let hitCard = [];
+	 for (i = 0; i < 2; i++) {
+		hitCard = deck.pop();
+		playerCards.push(hitCard);
+	}
+	
+	for (i = 0; i < 2; i++) {
+		hitCard = deck.pop();
+		dealerCards.push(hitCard);
+	}
+	// display the cards for the player
+	$(".playerCards").append(`<div class="playing-card ${playerCards[0]}"></div>`); // player's first card  playerCards[0]
+	$(".playerCards").append(`<div class="playing-card ${playerCards[1]}"></div>`); // player's second card  playerCards[1]
+	// display the cards for the dealer
+	$(".dealerCards").append(`<div class="playing-card ${dealerCards[0]}"></div>`); // dealer's first card  dealerCards[0]
+	$(".dealerCards").append(`<div class="playing-card back-blue" id="face-down-card"></div>`); // dealer's second card 
+	
+	$("#clear-button, #deal-button, .game-button, #double-button").toggle();
+	updateCount();
+	
+	if (playerCount === 21 && dealerCount < 21) {
+		playerBlackjack();
+	}
+	toggleSplit();
+ }  // end of dealTheHand function
+ 
+ // toggle the split button function
+ function toggleSplit() {
+  const firstCard = playerCards[0];
+  const secondCard = playerCards[1];
+  if (firstCard[firstCard.length - 1] === secondCard[secondCard.length - 1]) {
+	$("#split-button").toggle();
+  }
+ }
+ 
+ // split the cards function
+function splitTheHand () {
+	const secondCard = playerCards[1]; // second card of the player playerCards[1] 
+	$( `.playing-card.${ playerCards[1] }` ).remove(); // remove the second card of the player
+	splitCards = [secondCard]; // add the second card to the splitCards array
+	playersCards = playersCards.slice( 0, 1 ); // remove the second card from the playersCards array
+	$( ".splitCards" ).append( `<div class="playing-card ${ secondCard }"></div>` ); // display the second card of the player
+	$( "#split-button" ).toggle(); // toggle the split button
+	splitBet = playerBet; // set the splitBet to the playerBet
+	splitBetElement(); // display the splitBetElement
+	playerCardTake(); // take the player's cards
+	if ( playerCount === 21 && dealerCount < 21 ) {
+		playerBlackjack();
+	}
+	console.log( playerCount );
+	splitTakeCard();
+	if ( splitCount === 21 && dealerCount < 21 ) {
+		blackjack();
+		 
+	}
+	updateCount();
+	currentCards = "splitHand";
+}
+// double the bet function
+ function doubleTheBet () {
+	playerBet = playerBet * 2;
+	betElement();
+	cashELement();
+	$( "#deal-button, #clear-button, .form-inline" ).toggle();
+	dealTheHand();
+ }
+ 
+ // get the value of the hand function
+  function getHandValue (playerType) {
+   let hand = handOf(playerType); // get the hand of the player 
+   let count = 0; // set the count to 0
+   if (hand.length > 2) { // if the hand is greater than 2
+	   for (let i = 0; i < hand.length; i++) {
+           // for each card in the hand
+           let card = hand[i]; // set the card to the current card
+           let cardValue = card[0]; // set the cardValue to the first character of the card
+           count = cardValue === "A" && count < 11 ? 11 : cardValue === "A" && count > 11 ? 1 : cardValue === "J" || cardValue === "Q" || cardValue === "K" ? 10 : parseInt(cardValue);
+       }
+		   
+   }
+   return count; // return the count
+  }
+  function handOf(playerType) {
+   if (playerType === "player") {
+	 return playerCards;
+   } else if (playerType === "dealer") {
+	 return dealerCards;
+   } else if (playerType === "splitHand") {
+	 return splitCards;
+   }
+}
+
+// function to sort Aces 
+ function sortAces(hand) {
+	let aces = [];
+	let nonAces = [];
+	for (let i = 0; i < hand.length; i++) {
+		if (hand[i][0] === "A") {
+			aces.push(hand[i]);
+		} else {
+			nonAces.push(hand[i]);
+		}
+	}
+	return aces.concat(nonAces);
+}
+
+function playerCardTake () {
+	let cards = ( currentCards === "splitHand" ) ? splitTakeCard() : playerTakeCard();
+}
+  
+  // player take card function
+   function playerTakeCard() {
+	   let hitCard = deck.pop();
+	   playerCards.push(hitCard);
+	   $(".playerCards").append(`<div class="playing-card ${playersCards[playerCards.length - 1]}"></div>`);
+
+	   if ( playerCards.length > 2 ) {
+		   $( "#double-button, #split-button" ).hide();
+	   }
+	   	   updateCount();
+   }
+   // split take card function
+   function splitTakeCard() {
+	   let hitCard = deck.pop();
+	   splitCards.push(hitCard);
+	   $(".splitCards").append(`<div class="playing-card ${splitCards[splitCards.length - 1]}"></div>`);
+	   if ( splitCards.length > 2 ) {
+		   $( "#double-button, #split-button" ).hide();
+	   }
+	   updateCount();
+   }
+   
+   // stand function
+   function stand() {
+	   if ( splitCards.length < 1 ) {
+		   dealTheHand();
+		   scoreGame();
+		   $( "#double-button, #split-button" ).hide();
+	   } else if ( currentCards === "splitHand" ) {
+		   currentCards = "playerHand";
+		   $( "#double-button").show();
+		   toggleSplit();
+	   } else {
+		   currentCards = "splitHand";
+		   dealTheHand();
+		   scoreGame();
+		   $( "#double-button, #split-button" ).hide();
+	    }
+	    
+	  }function dealerHand() {
+	  // shows the face down card and the face up card of the dealer
+	   document.getElementById("face-down-card").className = `playing-card ${dealerCards[1]}`;
+	   // dealer takes cards until the dealer count is greater than 17
+	    if (dealerCount < 17) {
+			dealerTakeCard();
+			dealerHand();
+		}
+	  }
+	  
+	  // function to take a card for the dealer
+	  
+	   function dealerTakeCard() {
+	       hitCard = deck.pop(); // get the hitCard from the deck
+		   dealerCards.push(hitCard); // add the hitCard to the dealerCards array
+		   $(".dealerCards").append(`<div class="playing-card ${dealerCards[dealerCards.length - 1]}"></div>`); // display the hitCard
+		   dealerCount = getHandValue("dealer"); // get the value of the dealer's hand
+		   console.log(`Dealer count: ${dealerCount}`); // display the dealerCount
+		   if ( dealerCount < 17 ) { // if the dealer count is less than 17
+			   dealerTakeCard(); // take a card
+		   }
+	   }
+	   
+	   // function to score the game
+	    function Score() { 
+	     let count = ( currentCards === "splitHand" ) ? splitCount : playerCount;
+	     
+	     if 
+	    }
