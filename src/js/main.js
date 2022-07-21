fe35584dc6fb318438746888c92cd44ae026dc54
Eric-Peltzman33 <erpeltz@gmail.com>
2022-07-20 20:44:46 -0400

    bet and clear functions

--- src/js/main.js
+++ src/js/main.js
@@ -40,3 +40,63 @@ function splitBetElement () {
 
 // functions 
 
+function initial() {
+	deck = [];
+	playerCards = [];
+	splitCards = [];
+	dealerCards = [];
+	dealerCount = 0;
+	playerCount = 0;
+	splitCount = 0;
+	playerBet = 0;
+	splitBet = 0;
+	currentCards = "playerHand";
+	cashELement();
+	betElement();
+	document.getElementById("split-bet").innerHTML = "";
+	$(".playing-cards, .message").hide();
+	resetDeck()
+}
+
+// fill in the deck with cards
+function resetDeck () {
+	deck = [];
+	let cardSuits = ["h", "d", "s", "c"];
+	for ( let i = 0; i < cardSuits.length; i++ ) {
+		let cardValues = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+		for ( let j = 1; j < cardValues.length; j++ ) {
+			deck.push( cardValues[j] + cardSuits[i] );
+		}
+	}
+}
+ // shuffle the deck function
+  function shuffleDeck () {
+    	for ( let i = 0; i < deck.length; i++ ) {
+		let randomIndex = Math.floor( Math.random() * deck.length );
+		let temp = deck[i];
+		deck[i] = deck[randomIndex];
+		deck[randomIndex] = temp;
+	}
+}
+
+// make a bet function 
+function makeBet () {
+	playerBet = document.getElementById( "bet-amount" ).value;
+	if ( playerBet <= cash && playerBet > 0 ) {
+		betElement();
+		$( "#deal-button, #clear-button, .form-inline" ).toggle();
+	} else {
+		alert( "Please enter a valid bet amount" );
+	}
+}
+
+// clear the bet function
+function clearBet () {
+	betElement();
+	playerBet = 0;
+	document.getElementById( "bet-amount" ).value = "Enter bet amount: ";
+	document.getElementById( "submittedBet" ).innerHTML = "";
+	cashELement();
+	$( "#deal-button, #clear-button, .form-inline" ).toggle();
+}
+
